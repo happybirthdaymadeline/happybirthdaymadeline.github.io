229789836a3045d6b15b488d927ef066d85cf902
mcjcloud <brayden14cloud@gmail.com>
2015-11-05 16:25:05 -0600

    Added truefalse/updated riddle

--- mechanics.js
+++ mechanics.js
@@ -2,3 +2,18 @@ function redirect(url) {
 	window.location = url;
 }
 
+function submitAnswer() {
+    var answer = document.forms["form"]["answer"].value;
+    
+    if(answer.toLowerCase().search("kiss") > -1) {
+        window.alert("Correct<3 Your letter for the challenge is: N.");
+        redirect("http://happybirthdaymadeline.github.io");
+    }
+}
+
+function trueSubmit() {
+    window.alert("Oh good(: Here's your letter: Z");
+}
+function falseSubmit() {
+    window.alert("Oh :/ Well here's your letter. Q >:)");
+}
