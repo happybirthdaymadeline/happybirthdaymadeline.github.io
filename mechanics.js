2f5f60a6e4277d23948afd09f0d89b121ea80d5e
mcjcloud <brayden14cloud@gmail.com>
2015-11-06 14:23:21 -0600

    added two challenges

--- mechanics.js
+++ mechanics.js
@@ -9,6 +9,9 @@ function submitAnswer() {
         window.alert("Correct<3 Your letter for the challenge is: N.");
         redirect("http://happybirthdaymadeline.github.io");
     }
+    else {
+        window.alert("Sorry boo :* Try again?");
+    }
 }
 
 function trueSubmit() {
@@ -16,4 +19,53 @@ function trueSubmit() {
 }
 function falseSubmit() {
     window.alert("Oh :/ Well here's your letter. Q >:)");
+}
+
+function submitChoiceAnswer() {
+    if(document.getElementById("a4").checked == true) {
+        window.alert("Oh good you know(; And the percentage grows every birthday<3 Even more than infinity!\nHere's your letter: W");
+    }
+    else {
+        window.alert("Nope! Higher than that!");
+    }
+}
+
+function toggle(elem) {
+    var choice1 = document.getElementById("a1");
+    var choice2 = document.getElementById("a2");
+    var choice3 = document.getElementById("a3");
+    var choice4 = document.getElementById("a4");
+    
+    if(elem.id == "a1") {
+        choice2.checked = false;
+        choice3.checked = false;
+        choice4.checked = false;
+    }
+    else if(elem.id == "a2") {
+        choice1.checked = false;
+        choice3.checked = false;
+        choice4.checked = false;
+    }
+    else if(elem.id == "a3") {
+        choice1.checked = false;
+        choice2.checked = false;
+        choice4.checked = false;
+    }
+    else {
+        choice1.checked = false;
+        choice2.checked = false;
+        choice3.checked = false;
+    }
+}
+
+function submitQAnswer() {
+    
+    var message = document.getElementById("starbucksPref").value;
+    var subject = "From Madeline!";
+    
+    window.open("mailto:brayden14cloud@gmail.com?subject="
+    + encodeURIComponent(subject)
+    + "&body=" + encodeURIComponent(message));
+    
+    window.alert(document.location.href);
 }
